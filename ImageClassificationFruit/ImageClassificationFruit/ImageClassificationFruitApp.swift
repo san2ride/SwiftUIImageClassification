42a94ccfa6dc97353c53b7a71d16b12a0e2fd3f5
Jason <sanchezjason@me.com>
2024-01-29 14:44:45 -0700

    swiftui/fruit_image-MobileNetV2

--- ImageClassificationFruit/ImageClassificationFruit/ImageClassificationFruitApp.swift
+++ ImageClassificationFruit/ImageClassificationFruit/ImageClassificationFruitApp.swift
@@ -6,12 +6,74 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
-@main
-struct ImageClassificationFruitApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct ImageClassificationFruitApp: View {
+    
+    @State private var showSheet: Bool = false
+    @State private var showPhotoOptions: Bool = false
+    @State private var image: UIImage?
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    
+    let model = MobileNetV2()
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            VStack {
+                Spacer()
+                  Image(uiImage: image ?? UIImage(named: "placeholder")!)
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                
+                Button("Choose Picture") {
+                    // open action sheet
+                    self.showSheet = true
+                    
+                }.padding()
+                    .foregroundColor(Color.white)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .actionSheet(isPresented: $showSheet) {
+                        ActionSheet(title: Text("Select Photo"), message: Text("Choose"), buttons: [
+                            .default(Text("Photo Library")) {
+                                // open photo library
+                                self.showPhotoOptions = true
+                                self.sourceType = .photoLibrary
+                            },
+                            .default(Text("Camera")) {
+                                // open camera
+                                self.showPhotoOptions = true
+                                self.sourceType = .camera
+                            },
+                            .cancel()
+                        ])
+                        
+                }
+                
+                Spacer()
+                
+                Button("Classify") {
+                    
+                    // perform image classification
+                    
+                }.padding()
+                    .foregroundColor(Color.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                
+            }
+            .navigationBarTitle("Image Classification")
+        }.sheet(isPresented: $showPhotoOptions) {
+            ImagePicker(image: self.$image, isShown: self.$showPhotoOptions, sourceType: self.sourceType)
         }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageClassificationFruitApp()
     }
 }
---
+++ ImageClassificationFruit/ImageClassificationFruit/Preview Content/ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView.swift
+//  ImageClassificationFruit
+//
+//  Created by don't touch me on 1/29/24.
+//
+
+import Foundation
