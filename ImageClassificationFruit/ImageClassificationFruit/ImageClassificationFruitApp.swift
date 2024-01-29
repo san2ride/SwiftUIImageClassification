96234e5bbb8d84434d011c1a032fef936c341a36
Jason <sanchezjason@me.com>
2024-01-29 13:31:37 -0700

    swiftui/fruit_image-delagates

---
+++ ImageClassificationFruit/ImageClassificationFruit/ImageClassificationFruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationFruitApp.swift
+//  ImageClassificationFruit
+//
+//  Created by don't touch me on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationFruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
