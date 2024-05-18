9274992656b67ddad848b92fa620aa3d888b565b
Jason <sanchezjason@me.com>
2024-05-17 22:36:48 -0600

    swiftui/machine_learning/image_classification- RandomImageView

---
+++ RandomImageClassification/RandomImageClassification/RandomImageClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomImageClassificationApp.swift
+//  RandomImageClassification
+//
+//  Created by Jason Sanchez on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomImageClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RandomImageView()
+        }
+    }
+}
---
+++ RandomImageClassification/RandomImageClassification/RandomImageView.swift
@@ -0,0 +1,18 @@
+//
+//  RandomImageView.swift
+//  RandomImageClassification
+//
+//  Created by Jason Sanchez on 5/17/24.
+//
+
+import SwiftUI
+
+struct RandomImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomImageView()
+}
