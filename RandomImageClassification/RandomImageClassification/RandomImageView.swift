45f8d536d7e76682e3fbf8a0cb96d628d2ef1c7a
Jason <sanchezjason@me.com>
2024-05-20 12:42:08 -0600

    swiftui/machine_learning/image_classification- currentIndex, images

--- RandomImageClassification/RandomImageClassification/RandomImageView.swift
+++ RandomImageClassification/RandomImageClassification/RandomImageView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct RandomImageView: View {
+    
+    let images = ["1", "2", "3", "4"]
+    @State private var currentIndex =  0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(images[currentIndex])
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 225, height: 225)
+            HStack {
+                Button("Previous") {
+                    currentIndex -= 1
+                }.buttonStyle(.bordered)
+                    .disabled(currentIndex == 0)
+                
+                Button("Next") {
+                    currentIndex += 1
+                }.buttonStyle(.bordered)
+                    .disabled(currentIndex == images.count - 1)
+            }
+        }
+        .padding()
     }
 }
 
